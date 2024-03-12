8abd4a582532893c99c40e66fd91a470063a403d
Criafge <110690865+criafge@users.noreply.github.com>
2024-03-12 10:33:32 +0500

    index page

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\News;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
     public function __invoke(Request $request)
     {
-        return view('index');
+
+        return view('index', ['new' => News::latest()->limit(12)->paginate(4)]);
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-
-</div>
+    <div class="container">
+        <div class="d-flex flex-column gap-3">
+            <h1>Последние новости</h1>
+            <div class="d-flex gap-5">
+                @foreach ($new as $item)
+                    <div class="card text-bg-dark mb-3" style="max-width: 18rem;">
+                        <div class="card-header">{{ $item->created_at }}</div>
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $item->title }}</h5>
+                            <p class="card-text">{{ $item->content }}</p>
+                            <a href="news/{{$item->id}}">Читать</a>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+            {{ $new->links('pagination::bootstrap-5') }}
+        </div>
+    </div>
 @endsection
